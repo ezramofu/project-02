1b9f09428aedb21a2dde3231d149409af772fc03
ezramofu <91767244+ezramofu@users.noreply.github.com>
2022-03-19 10:09:48 +0900

    add project 02

---
+++ loops.php
@@ -0,0 +1,30 @@
+<?php 
+//while
+//while (true){
+//      //Do somethings
+//}
+//loop with $counter
+// $counter = 0;
+// $loop = true;
+// while ($loop){
+//     echo "Counter $counter", '<br>';
+//     if($counter >= 10)
+//         $loop = false;
+//     $counter++;
+// }
+//do - while
+// $counter = 10;
+// do {
+//     echo  "Counter $counter";
+// } while ($counter < 10);
+
+//for
+for ($i = 1; $i < 10; $i++){
+    echo "Counter: $i".'<br>';
+}
+//foreach
+
+//literate Over associative array
+
+
+?>
